0f69c0e90ffe1c10051374a42c6aff1b7388bbfa
Fernando Ribeiro <fernandoribeiro@mpf.mp.br>
2018-05-10 17:23:22 -0300

    Creating Student entity

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,4 @@
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -23,3 +23,8 @@ class Class(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    school = models.CharField(max_length=50, blank=True)
